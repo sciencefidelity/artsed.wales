c0a7b08e84284ba1b9c9a61fe3f6322beac57a29
Matt Cook <32623301+sciencefidelity@users.noreply.github.com>
2021-02-21 15:57:40 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 
+const buttonElement = <button className="Btn">CY</button>;
+
+const greeting = {
+  en: 'Hello World',
+  cy: 'Helo Byd' 
+};
+
+const message = <h1>{greeting.en}!</h1>;
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Hello World!
-        </h1>
+        {buttonElement}
+        {message}
       </header>
     </div>
   );
