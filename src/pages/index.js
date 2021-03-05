7b83be1ee9b57fc50ea565307ad411297f1f166b
Matt Cook <32623301+sciencefidelity@users.noreply.github.com>
2021-03-05 15:53:25 +0000

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="National Arts and Education Network" />
+  </Layout>
+)
+
+export default IndexPage
