5d08d6350f3e2a82ef8438b48e65855c68357380
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-03-14 23:47:59 +0000

    Set up 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,36 +1,16 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
+    <Layout>
+    <main>
       <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
+      <h1>Page not found</h1>
+      <p>
         Sorry{" "}
         <span role="img" aria-label="Pensive emoji">
           😔
@@ -40,7 +20,7 @@ const NotFoundPage = () => {
         {process.env.NODE_ENV === "development" ? (
           <>
             <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
+            Try creating a page in <code>src/pages/</code>.
             <br />
           </>
         ) : null}
@@ -48,6 +28,7 @@ const NotFoundPage = () => {
         <Link to="/">Go home</Link>.
       </p>
     </main>
+    </Layout>
   )
 }
 
