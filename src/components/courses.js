162bd1a7a9707af492be8b9d621ae7abac495699
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-03-16 16:05:02 +0000

    Add css module for courses card

--- src/components/courses.js
+++ src/components/courses.js
@@ -1,22 +1,6 @@
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
-
-const briteLink = {
-  display: `flex`,
-  background: `#0D0D0D`,
-  height: `3.5rem`,
-  border: 0,
-  margin: `2.9rem auto 1.7rem`,
-  color: `white`,
-  fontSize: `1.8rem`,
-  fontFamily: `"Neue Haas Unica",-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-  Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif`,
-  fontWeight: `bold`,
-  padding: `0.2rem 1.5rem 0.2rem`,
-  borderRadius: `0.5rem`,
-  boxShadow: `0 0.3rem 0px rgb(156, 158, 158)`,
-  cursor: `pointer`
-}
+import * as courseStyles from "../css/course.module.css"
 
 const Courses = ({
   site1, site2a, site2b, site2c, site3, site4, book,
@@ -28,127 +12,63 @@ const Courses = ({
       display: `flex`
     }}
   >
-    <div
-      style={{
-        display: `flex`,
-        flexDirection: `column`,
-        border: `8px solid white`,
-        width: `46%`,
-        marginRight: `auto`,
-        boxShadow: `2px 2px 10px rgba(0, 0, 0, 0.2)`,
-      }}
-    >
+    <div className={courseStyles.cardContainer}>
       <StaticImage
         src="../images/aen_outdoor_2.jpg"
         width={400}
         height={600}
         quality={80}
         formats={["AUTO", "WEBP", "AVIF"]}
-        alt="War and Peace Exhibition at Blackwood Miners Institute"
-
+        alt=""
       />
-      <div
-        style={{
-          width: `100%`,
-          padding: `1rem 1.3rem 1rem 1.3rem`,
-          position: `relative`,
-        }}
-      >
+      <div className={courseStyles.cardText}>
         <h2
           style={{
-            letterSpacing: `-0.01rem`,
+            letterSpacing: `-0.01rem`, // remove for English text
           }}
         >{site1}</h2>
-        <h3
-          style={{
-            margin: `0 0 0.8rem`,
-            fontSize: `1.5rem`,
-            fontWeight: 400
-          }}
+        <h3 
+          className={courseStyles.courseDate}
         >{site2a}<sup>{site2b}</sup> {site2c}</h3>
         <h4
-          style={{
-            margin: `0.1rem 0`,
-            fontSize: `1.2rem`,
-            fontWeight: 400
-          }}
-        >
-          {site3}
-        </h4>
+          className={courseStyles.courseLocation}
+        >{site3}</h4>
         <h4
-          style={{
-            margin: `0.4rem 0 2rem`,
-            fontSize: `1.2rem`,
-            fontWeight: 400
-          }}
-        >
-          £65
-        </h4>
+          className={courseStyles.coursePrice}
+        >£65</h4>
         <p>{site4}</p>
         <a 
           href="https://eventbrite.com/"
           target="_blank"
           rel="noreferrer"
         >
-          <button style={briteLink}>{book}</button>
+          <button className={courseStyles.britelink}>{book}</button>
         </a>
       </div>
     </div>
-    <div
-      style={{
-        display: `flex`,
-        flexDirection: `column`,
-        border: `8px solid white`,
-        width: `46%`,
-        marginLeft: `auto`,
-        boxShadow: `2px 2px 10px rgba(0, 0, 0, 0.2)`,
-      }}
-    >
+    <div className={courseStyles.cardContainer}>
       <StaticImage
         src="../images/aen_printing.jpg"
         width={400}
         height={600}
         quality={80}
         formats={["AUTO", "WEBP", "AVIF"]}
-        alt="War and Peace Exhibition at Blackwood Miners Institute"
-  
+        alt=""
       />
-      <div
-        style={{
-          width: `100%`,
-          padding: `1rem 1.3rem 1rem 1.3rem`,
-          position: `relative`,
-        }}
-      >
+      <div className={courseStyles.cardText}>
         <h2>{digital1}</h2>
-        <h3
-          style={{
-            margin: `0 0 0.8rem`,
-            fontSize: `1.5rem`,
-            fontWeight: 400
-          }}
+        <h3 
+          className={courseStyles.courseDate}
         >{digital2a}<sup>{digital2b}</sup> {digital2c}</h3>
         <h4
-          style={{
-            margin: `0.1rem 0`,
-            fontSize: `1.2rem`,
-            fontWeight: 400
-          }}
-        >
-          {digital3}
-        </h4>
+          className={courseStyles.courseLocation}
+        >{digital3}</h4>
         <h4
-          style={{
-            margin: `0.4rem 0 2rem`,
-            fontSize: `1.2rem`,
-            fontWeight: 400
-          }}
-        >
-          £65
-        </h4>
+          className={courseStyles.coursePrice}
+        >£65</h4>
         <p
           style={{
-            letterSpacing: `-0.01rem`
+            letterSpacing: `-0.01rem` // remove for English text
           }}
         >{digital4}</p>
         <a 
@@ -156,7 +76,9 @@ const Courses = ({
           target="_blank"
           rel="noreferrer"
         >
-          <button style={briteLink}>{book}</button>
+          <button 
+            className={courseStyles.britelink}
+          >{book}</button>
         </a>
       </div>
     </div>
