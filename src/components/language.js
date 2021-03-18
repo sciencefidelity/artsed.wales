0226f952eedac2a2f11da74084c14fce15d7aac5
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-03-18 21:05:50 +0000

    Completed header responsivity

--- src/components/language.js
+++ src/components/language.js
@@ -3,35 +3,21 @@ import { IntlContextConsumer, changeLocale } from "gatsby-plugin-intl"
 
 const languageName = {
   en: "EN",
-  cy: "CY",
+  cy: "CY"
 }
 
 const Language = () => {
   return (
-    <div 
-      style={{
-        display: `flex`,
-        fontFamily: `"Neue Haas Unica",-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif`,
-        textTransform: `uppercase`,
-        alignItems: `center`,
-        fontSize: `3.5rem`,
-        letterSpacing: `0.07rem`,
-        marginLeft: `auto`,
-        marginRight: 40,
-      }}
-    >
+    <div className="languageSwitcher">
       <IntlContextConsumer>
         {({ languages, language: currentLocale }) =>
           languages.map(language => (
             <a
               key={language}
               onClick={() => changeLocale(language)}
+              className="languageLink"
               style={{
-                color: `#e7dbd8`,
-                display: currentLocale === language ? `none` : `flex`,
-                cursor: `pointer`,
-                userSelect: `none`
+                display: currentLocale === language ? `none` : `flex`
               }}
             >
               {languageName[language]}
