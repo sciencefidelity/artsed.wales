e57467212511b770fcb6af3254226e6b72c55c27
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-08 21:32:14 +0000

    Try moving illustrations to separate components

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -1,33 +1,38 @@
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 
+import IllustrationA from "./illustrations/illustrationA"
+
 const Introduction = ({ introText1, introText2 }) => (
-  <section className="introSection">
-    <div className="splitContainer">
-      <div className="splitText">
-        <p
-          style={{
-            letterSpacing: `-0.02rem` // remove for English
-          }}
-        >{introText1}</p>
-        <p
-          style={{
-            letterSpacing: `-0.01rem` // remove for English
-          }}
-        >{introText2}</p>
+  <>
+    <IllustrationA />
+    <section className="introSection">
+      <div className="splitContainer">
+        <div className="splitText">
+          <p
+            style={{
+              letterSpacing: `-0.02rem` // remove for English
+            }}
+          >{introText1}</p>
+          <p
+            style={{
+              letterSpacing: `-0.01rem` // remove for English
+            }}
+          >{introText2}</p>
+        </div>
+        <StaticImage
+          src="../images/aen_blackwood_1.jpg"
+          width={600}
+          height={400}
+          quality={80}
+          objectPosition={"50% 50%"}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt=""
+          className="sideImage"
+        />   
       </div>
-      <StaticImage
-        src="../images/aen_blackwood_1.jpg"
-        width={600}
-        height={400}
-        quality={80}
-        objectPosition={"50% 50%"}
-        formats={["AUTO", "WEBP", "AVIF"]}
-        alt=""
-        className="sideImage"
-      />   
-    </div>
-  </section>
+    </section>
+  </>
 )
 
 export default Introduction
