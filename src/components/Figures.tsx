1b74303318e788221d8a460cdf8c62cff57bd522
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-05-26 00:41:24 +0000

    Add amimated countup

--- src/components/Figures.tsx
+++ src/components/Figures.tsx
@@ -1,6 +1,8 @@
 // Figures section with auto countup
 
 import React from "react"
+import CountUp from 'react-countup'
+import VisibilitySensor from 'react-visibility-sensor'
 
 import IllustrationD from "./illustrations/illustrationD"
 
@@ -10,19 +12,43 @@ const Figures = ({ legacy1, legacy2, legacy3, legacy4, legacy5 }) => (
     <div className="legacySmall"><small>{legacy1}</small></div>
     <div className="figuresLayout">
       <div>
-        <h3 className="figure">782</h3>
+        <CountUp end={782} redraw={true}>
+          {({ countUpRef, start }) => (
+            <VisibilitySensor onChange={start} delayedCall>
+              <div className="figure" ref={countUpRef} />
+            </VisibilitySensor>
+          )}
+        </CountUp>
         <h4 className="figureCap">{legacy2}</h4>
       </div>
       <div>
-        <h3 className="figure">2351</h3>
+        <CountUp end={2351} redraw={true}>
+          {({ countUpRef, start }) => (
+            <VisibilitySensor onChange={start} delayedCall>
+              <div className="figure" ref={countUpRef} />
+            </VisibilitySensor>
+          )}
+        </CountUp>
         <h4 className="figureCap">{legacy3}</h4>
       </div>
       <div>
-        <h3 className="figure">302</h3>
+        <CountUp end={302} redraw={true}>
+          {({ countUpRef, start }) => (
+            <VisibilitySensor onChange={start} delayedCall>
+              <div className="figure" ref={countUpRef} />
+            </VisibilitySensor>
+          )}
+        </CountUp>
         <h4 className="figureCap">{legacy4}</h4>
       </div>
       <div>
-        <h3 className="figure">10895</h3>
+        <CountUp end={10895} redraw={true}>
+          {({ countUpRef, start }) => (
+            <VisibilitySensor onChange={start} delayedCall>
+              <div className="figure" ref={countUpRef} />
+            </VisibilitySensor>
+          )}
+        </CountUp>
         <h4 className="figureCap">{legacy5}</h4>
       </div>
     </div>
