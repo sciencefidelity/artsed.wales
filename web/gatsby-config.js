a34c35d56ae5a35b454264609b456eef29570cc7
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-04 22:57:01 +0000

    refactor: standardise quotes

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
+require("dotenv").config()
 
-const clientConfig = require('./client-config')
+import clientConfig from "./client-config"
 
 module.exports = {
   siteMetadata: {
@@ -10,7 +10,7 @@ module.exports = {
       "We bring together schools, artists and cultural organisations to succeed in the Expressive Arts.",
     url: "https://nat.artsed.wales",
     image: "/static/images/aen_network.jpg",
-    twitterUsername: "@artsed_wales",
+    twitterUsername: "@artsed_wales"
   },
   plugins: [
     "gatsby-plugin-image",
@@ -18,7 +18,7 @@ module.exports = {
     "gatsby-transformer-remark",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    `gatsby-plugin-sass`,
+    "gatsby-plugin-sass",
     {
       resolve: "gatsby-plugin-eslint",
       options: {
@@ -27,53 +27,53 @@ module.exports = {
         // Default settings that may be ommitted or customized
         stages: ["develop"],
         extensions: ["js", "jsx", "ts", "tsx"],
-        exclude: ["node_modules", "bower_components", ".cache", "public"],
+        exclude: ["node_modules", "bower_components", ".cache", "public"]
         // Any additional eslint-webpack-plugin options below
         // ...
-      },
+      }
     },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        short_name: `naen`,
-        start_url: `/`,
-        background_color: `#e7dbd8`,
-        theme_color: `#e7dbd8`,
-        display: `minimal-ui`,
-        icon: "src/icons/icon.png",
-      },
+        short_name: "naen",
+        start_url: "/",
+        background_color: "#e7dbd8",
+        theme_color: "#e7dbd8",
+        display: "minimal-ui",
+        icon: "src/icons/icon.png"
+      }
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: "./src/images/",
+        path: "./src/images/"
       },
-      __key: "images",
+      __key: "images"
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "pages",
-        path: "./src/pages/",
+        path: "./src/pages/"
       },
-      __key: "pages",
+      __key: "pages"
     },
     {
-      resolve: `gatsby-source-sanity`,
+      resolve: "gatsby-source-sanity",
       options: {
         ...clientConfig.sanity
-      },
+      }
     },
     {
-      resolve: `gatsby-plugin-react-intl`,
+      resolve: "gatsby-plugin-react-intl",
       options: {
-        path: `${__dirname}/src/intl`,
-        languages: [`en`, `cy`],
-        defaultLanguage: `en`,
+        path: "${__dirname}/src/intl",
+        languages: ["en", "cy"],
+        defaultLanguage: "en",
         redirect: true,
-        redirectComponent: require.resolve(`./src/components/redirect.tsx`),
-      },
-    },
-  ],
-};
+        redirectComponent: require.resolve("./src/components/redirect.tsx")
+      }
+    }
+  ]
+}
