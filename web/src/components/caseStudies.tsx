5e422d304cde6ee9c2a4d130072d7279caf3e0e4
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-07 16:34:09 +0000

    fix: re-add files that have had the case messed up

---
+++ web/src/components/caseStudies.tsx
@@ -0,0 +1,44 @@
+// Three row section with headings Art, Music, Theatre
+
+import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+
+import IllustrationC from "./illustrations/illustrationC"
+
+const CaseStudies = ({ art1, art2, music1, music2, theatre1, theatre2 }) => (
+  <>
+    <section
+      style={{
+        marginBottom: "6rem"
+      }}
+    >
+      <IllustrationC />
+      <div className="thirdsLayout">
+        <div>
+          <h2>{art1}</h2>
+          <p>{art2}</p>
+        </div>
+        <div>
+          <h2>{music1}</h2>
+          <p>{music2}</p>
+        </div>
+        <div>
+          <h2>{theatre1}</h2>
+          <p>{theatre2}</p>
+        </div>
+      </div>
+      <StaticImage
+        src="../images/aen_theatre.jpg"
+        width={600}
+        height={400}
+        quality={80}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt=""
+        className="centeredImage"
+        style={{ display: "block" }}
+      />
+    </section>
+  </>
+)
+
+export default CaseStudies
