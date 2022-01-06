5ae80e475df69e4979e5c5ff5ae34400bba13f3d
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-06 23:50:36 +0000

    chore: remove boilerplate

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -6,15 +6,7 @@
 </script>
 
 <script lang="ts">
-  import { onMount } from "svelte"
 
-  let count = 0
-  onMount(() => {
-    const interval = setInterval(() => count++, 1000)
-    return () => {
-      clearInterval(interval)
-    }
-  })
 </script>
 
 <svelte:head>
