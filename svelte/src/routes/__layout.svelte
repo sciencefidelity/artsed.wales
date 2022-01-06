d68d9455fa6a3523edbc09d4cf1c2e9aa48e18d2
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-06 23:38:31 +0000

    feat: change svelte-i18n to sveltekit-i18n

--- svelte/src/routes/__layout.svelte
+++ svelte/src/routes/__layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-  import Footer from "$lib/Footer.svelte"
-  import Header from "$lib/Header.svelte"
   import "sanitize.css/sanitize.css"
   import "sanitize.css/assets.css"
   import "sanitize.css/reduce-motion.css"
   import "sanitize.css/typography.css"
   import "../styles/app.scss"
+  import { waitLocale } from 'svelte-i18n'
+  import Footer from "$lib/Footer.svelte"
+  import Header from "$lib/Header.svelte"
+  export async function preload() {
+    return waitLocale()
+  }
 </script>
 
 <Header />
