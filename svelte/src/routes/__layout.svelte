68bf97150119818ed3a6cf74b9af31ce3070a671
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-06 23:46:34 +0000

    refactor: move icon out of component and remove i18n imports

--- svelte/src/routes/__layout.svelte
+++ svelte/src/routes/__layout.svelte
@@ -4,12 +4,8 @@
   import "sanitize.css/reduce-motion.css"
   import "sanitize.css/typography.css"
   import "../styles/app.scss"
-  import { waitLocale } from 'svelte-i18n'
   import Footer from "$lib/Footer.svelte"
   import Header from "$lib/Header.svelte"
-  export async function preload() {
-    return waitLocale()
-  }
 </script>
 
 <Header />
