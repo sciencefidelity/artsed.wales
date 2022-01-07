10e4538564850739d031d6f5c21636566ac5c385
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-07 22:18:33 +0000

    chore: add full path to i18n

--- svelte/src/routes/__layout.svelte
+++ svelte/src/routes/__layout.svelte
@@ -1,5 +1,5 @@
 <script context="module">
-  import { loadTranslations } from "$lib/translations"
+  import { loadTranslations } from "$lib/translations/index.js"
 
   export const load = async ({ url }) => {
     const { pathname } = url
