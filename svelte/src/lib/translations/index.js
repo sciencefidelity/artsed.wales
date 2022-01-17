aab3ff4072441fe2a68f7a511e8c6c7094ccf0f2
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-17 14:13:37 +0000

    refactor: add translation routs and update translations

--- svelte/src/lib/translations/index.js
+++ svelte/src/lib/translations/index.js
@@ -2,34 +2,49 @@ import i18n from "sveltekit-i18n"
 import lang from "./lang.json"
 
 export const config = {
+  translations: {
+    en: { lang },
+    cy: { lang }
+  },
   loaders: [
     {
       locale: "en",
-      key: "lang",
-      loader: () => lang
-    },
-    {
-      locale: "en",
       key: "content",
+      routes: ["", "/"],
       loader: async () => (await import("./en/content.json")).default
     },
     {
-      locale: "cy",
-      key: "lang",
-      loader: () => lang
+      locale: "en",
+      key: "error",
+      routes: ["error"],
+      loader: async () => (await import("./en/error.json")).default
     },
     {
       locale: "cy",
       key: "content",
+      routes: ["", "/"],
       loader: async () => (await import("./cy/content.json")).default
+    },
+    {
+      locale: "cy",
+      key: "error",
+      routes: ["error"],
+      loader: async () => (await import("./cy/error.json")).default
     }
   ]
 }
 
-export const { t, loading, locales, locale, loadTranslations } = new i18n(
-  config
-)
+export const defaultLocale = "en"
 
-loading.subscribe(
-  $loading => $loading && console.log("Loading translations...")
-)
+export const { t, locale, locales, loading, loadTranslations, translations } =
+  new i18n(config)
+
+// Translations logs
+loading.subscribe(async $loading => {
+  if ($loading) {
+    console.log("Loading translations...")
+
+    await loading.toPromise()
+    console.log("Updated translations", translations.get())
+  }
+})
--- svelte/src/routes/__error.svelte
+++ svelte/src/routes/__error.svelte
@@ -14,18 +14,18 @@
   export const prerender = true
 </script>
 
+<script lang="ts">
+  import { t } from "$lib/translations/index.js"
+</script>
+
 <svelte:head>
-  <title>Not Found</title>
+  <title>{$t("error.header")}</title>
 </svelte:head>
 
 <div class="four-oh-four">
-  <h1>404 Not Found</h1>
-  <p>
-    You've hit a path that does not exist,
-    <br />
-    oh the sadness!
-  </p>
-  <a sveltekit:prefetch href="/">Home</a>
+  <h1>{$t("error.header")}</h1>
+  <p>{$t("error.description")}</p>
+  <a sveltekit:prefetch href="/">{$t("error.link")}</a>
 </div>
 
 <!-- prettier-ignore -->
