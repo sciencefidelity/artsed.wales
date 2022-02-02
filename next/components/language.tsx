fad5e6935cb50bcdc525a6bd83ead8f94c39cfdc
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-02 09:57:36 +0000

    feat: create language switcher

--- next/components/language.tsx
+++ next/components/language.tsx
@@ -1,20 +1,42 @@
 import { useRouter } from "next/router"
+import { capitalize } from "lib/utils"
 import Link from "components/link"
 import u from "styles/utils.module.scss"
 import s from "components/language.module.scss"
 
 const Language = () => {
-  const { pathname, asPath, query, locale } = useRouter()
+  const router = useRouter()
+  const { pathname, asPath, query, locale, locales } = router
+  const languages = ["cymreag", "english"]
   return (
     <nav className={`${u.nav} ${u.flex} ${u.column} ${u.mlAuto}`}>
       <div className={`${u.navItem} ${u.h1} ${u.sans}`}>
-        <Link href=""
-          className={`${s.navLink} ${u.fgLight}`}
-        >
-          <div className={`${u.inlineBlock} ${u.arrowLeft}`}>
-
-          </div>
-        </Link>
+        <div className="languageSwitcher">
+          <span className="screenReaderText">
+            {locale === "cy"
+              ? "Switch language to English"
+              : "Newid iaith i'r Gymraeg"}
+          </span>
+          {locale === "cy" ? (
+            <span
+              className={`${s.navLink} ${u.fgLight}`}
+              onClick={() => {
+                router.push({ pathname, query }, asPath, { locale: locales[0] })
+              }}
+            >
+              {capitalize(languages[1])}
+            </span>
+          ) : (
+            <span
+              className={`${s.navLink} ${u.fgLight}`}
+              onClick={() => {
+                router.push({ pathname, query }, asPath, { locale: locales[1] })
+              }}
+            >
+              {capitalize(languages[0])}
+            </span>
+          )}
+        </div>
       </div>
       <div className={`${u.navItem} ${u.h1} ${u.sans}`}>
         <Link href="/courses"
