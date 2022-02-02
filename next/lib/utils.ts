edebcb88571eb59baca540cc026e34d2e0c0bf1e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-02 09:49:19 +0000

    refactor: prettify

--- next/lib/utils.ts
+++ next/lib/utils.ts
@@ -2,36 +2,31 @@ import imageUrlBuilder from "@sanity/image-url"
 import sanityClient from "@/lib/sanityClient"
 import { Image } from "@/lib/interfaces"
 
-export const acronym = (title: string): string => {
+export const acronym = (title: string) => {
   return title.match(/[A-Z]/g).join("")
 }
 
-export const capitalize = (word: string): string =>
-  word[0].toUpperCase() + word.slice(1, word.length)
+export const capitalize = (word: string) => {
+  return word[0].toUpperCase() + word.slice(1, word.length)
+}
 
-export const dateOptions = {
-  weekday: "long",
-  year: "numeric",
-  month: "long",
-  day: "numeric"
-} as const
+export const kebabCase = (word: string) => {
+  // eslint-disable-next-line no-useless-escape
+  return word.toLowerCase().split(" ").join("-").replace(/[^a-z0-9\-]/g, "")
+}
 
-export const kebabCase = (word: string): string =>
-  word
-    .toLowerCase()
-    .split(" ")
-    .join("-")
-    .replace(/[^a-z0-9\-]/g, "")
-
-export const urlFor = (source: Image) =>
+export const urlFor = (source: Image) => {
   imageUrlBuilder(sanityClient).image(source)
+}
 
-//https://stackoverflow.com/questions/5845238/javascript-generate-transparent-1x1-pixel-in-dataurl-format/33919020#33919020
+// https://stackoverflow.com/questions/5845238/javascript-generate-transparent-1x1-pixel-in-dataurl-format/33919020#33919020
 
-export const hexDataURL = (hexColor: string) =>
-  `data:image/gif;base64,R0lGODlhAQABAPAA${encodeHex(hexColor)}/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
+export const hexDataURL = (hexColor: string) => {
+  return `data:image/gif;base64,R0lGODlhAQABAPAA${encodeHex(hexColor)}/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
+}
 
-const keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+const keyStr =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
 
 const encodeHex = (hexColor: string) => {
   const s = hexColor.substring(1, 7)
@@ -44,9 +39,12 @@ const encodeRGB = (r: number, g: number, b:number) => {
 }
 
 const triplet = (e1: number, e2: number, e3: number) => {
-  const enc1 = e1 >> 2;
+  const enc1 = e1 >> 2
   const enc2 = ((e1 & 3) << 4) | (e2 >> 4)
   const enc3 = ((e2 & 15) << 2) | (e3 >> 6)
-  const enc4 = e3 & 63;
-  return keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4)
+  const enc4 = e3 & 63
+  return keyStr.charAt(enc1)
+    + keyStr.charAt(enc2)
+    + keyStr.charAt(enc3)
+    + keyStr.charAt(enc4)
 }
