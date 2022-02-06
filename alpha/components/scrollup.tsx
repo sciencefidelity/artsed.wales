e01420b507ff3c4783a38e389b30ce90a3b0084f
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-06 20:20:11 +0000

    feat: add a logo in the corner

--- alpha/components/scrollup.tsx
+++ alpha/components/scrollup.tsx
@@ -1,23 +1,26 @@
 import { useRouter } from "next/router"
+import CornerAmpersand from "components/cornerAmpersand"
 import Link from "components/link"
+import s from "components/scrollup.module.scss"
+import u from "styles/utils.module.scss"
 // TODO: screen reader text hard coded
 const Scrollup = () => {
   const { locale } = useRouter()
   return (
-    <Link href="#">
-      <div className="scrollupContainer">
-        <span className="screenReaderText">
+    <div className={s.scrollupContainer}>
+      <Link href="#">
+        <span className={u.screenReaderText}>
           {locale === "cy" ? "Yn ôl i'r brig" : "Back to top"}
         </span>
-        <img
+        <CornerAmpersand />
+{/*         <img
           alt=""
           src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' class='scroll-up' viewBox='0 0 104 54'%3E%3Cpath d='M51.7 5l-48.1 48.1c-0.8 0.8-2.1 0.8-2.9 0 -0.8-0.8-0.8-2.1 0-2.9l49.6-49.6c0.8-0.8 2.1-0.8 2.9 0l49.6 49.6c0.8 0.8 0.8 2.1 0 2.9 -0.8 0.8-2.1 0.8-2.9 0l-48.1-48.1Z' fill='%23999999'/%3E%3C/svg%3E"
           loading="lazy"
           className="scrollup"
-        />
-      </div>
-    </Link>
+        /> */}
+      </Link>
+    </div>
   )
 }
-
 export default Scrollup
