56e36d0187aab3c4f4cce09aed47553f618107db
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-09 08:07:56 +0000

    refactor: remove unused date component and replace with postDate component

--- alpha/components/date.tsx
+++ alpha/components/date.tsx
@@ -1,6 +1,23 @@
-import { parseISO, format } from "date-fns"
-
-export default function Date({ dateString }: { dateString: string }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>
+import { useRouter } from "next/router"
+import format from "date-fns/format"
+import { cy, enGB } from "date-fns/locale"
+// Saturday, 12 February 2022, 14:00
+// dydd Sadwrn, 12 Chwefror 2022, 14:00
+const PostDate = ({ date }: { date: string }) => {
+  const { locale } = useRouter()
+  return (
+    <>
+      {locale === "cy"
+        ? format(new Date(date),
+          "eeee, d MMMM yyyy, HH:mm",
+          {locale: cy}
+        )
+        : format(new Date(date),
+          "eeee, d MMMM yyyy, HH:mm",
+          {locale: enGB}
+        )
+      }
+    </>
+  )
 }
+export default PostDate
