bc37f9420cccc107d2d1b64c8b9da41092457797
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-04 09:56:43 +0000

    refactor: testing mailchimp api

--- alpha/pages/api/mailchimp.ts
+++ alpha/pages/api/mailchimp.ts
@@ -7,15 +7,13 @@ mailchimp.setConfig({
 
 export default async function subscribe(req, res) {
   const email = req.body.email
-  const FNAME = req.body.firstName || ""
-  const LNAME = req.body.lastName || ""
-  const response = await mailchimp.lists.addListMember("dc2f9bfec3", {
-    email_address: email,
-    status: "pending",
-    merge_fields: {
-      FNAME: FNAME,
-      LNAME: LNAME
-    }
+  // const FNAME = req.body.firstName || ""
+  // const LNAME = req.body.lastName || ""
+  const response = await mailchimp.lists.addListMember(
+    "dc2f9bfec3",
+    true,
+    {
+      email_address: email
   })
   console.log(response)
   res.status(200).json({ response })
