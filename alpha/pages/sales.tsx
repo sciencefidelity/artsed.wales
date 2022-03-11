da8ba95b6e9ec9122eeecf2a9de713ac7d333fb4
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-11 15:25:24 +0000

    feat: draw a pie chart with sales data

--- alpha/pages/sales.tsx
+++ alpha/pages/sales.tsx
@@ -1,14 +1,33 @@
+import PieChart from "components/pieChart"
 import * as data from "../mock_data/mockEventData.json"
 
 const Sales = () => {
-  const dataRemaining = data.map(e => {
-    return Object.assign(e, {
-      remaining: e.capacity - e.quantity_sold_en - e.quantity_sold_cy
-    })
-  })
+  const totalSales = data[3].quantity_sold_en - data[3].quantity_sold_cy
+  const pieData = [
+    {
+      label: "English",
+      value: data[3].quantity_sold_en
+    },
+    {
+      label: "Welsh",
+      value: data[3].quantity_sold_cy
+    },
+    {
+      label: "Remaining",
+      value: data[3].capacity - totalSales
+    }
+  ]
   return (
-    <div>
-      {JSON.stringify(dataRemaining)}
+    <div style={{
+      margin: "auto",
+      width: "100%"
+    }}>
+      <PieChart
+        data={pieData}
+        innerRadius={50}
+        outerRadius={100}
+        title={data[3].name_en}
+      />
     </div>
   )
 }
