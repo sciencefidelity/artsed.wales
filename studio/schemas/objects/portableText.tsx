a44e0101b7341ac361a9a026e735d0f90ec5d547
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-12 15:47:20 +0000

    fix: change jsx attributes to camel case

--- studio/schemas/objects/portableText.tsx
+++ studio/schemas/objects/portableText.tsx
@@ -60,7 +60,7 @@ export default {
                 <svg
                   stroke="currentColor"
                   fill="currentColor"
-                  stroke-width="0"
+                  strokeWidth="0"
                   viewBox="0 0 24 24"
                   height="1em"
                   width="1em"
@@ -97,7 +97,7 @@ export default {
                 <svg
                   stroke="currentColor"
                   fill="currentColor"
-                  stroke-width="0"
+                  strokeWidth="0"
                   viewBox="0 0 24 24"
                   height="1em"
                   width="1em"
@@ -126,7 +126,7 @@ export default {
                 <svg
                   stroke="currentColor"
                   fill="currentColor"
-                  stroke-width="0"
+                  strokeWidth="0"
                   viewBox="0 0 24 24"
                   height="1em"
                   width="1em"
