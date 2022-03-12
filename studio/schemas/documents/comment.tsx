c38b5e2eda69608ae46ca95228afb6173e60ac0b
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-12 15:44:03 +0000

    fix: rename stroke-width for jsx

--- studio/schemas/documents/comment.tsx
+++ studio/schemas/documents/comment.tsx
@@ -13,7 +13,7 @@ export default {
     <svg
       stroke="currentColor"
       fill="currentColor"
-      stroke-width="0"
+      strokeWidth="0"
       viewBox="0 0 16 16"
       height="1em"
       width="1em"
