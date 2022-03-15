d0ea519677dae72a0c10281d6f607b444386c19e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-15 15:27:39 +0000

    refactor: replace svg icons with react-icons

--- studio/src/schemas/documents/newsletter.tsx
+++ studio/src/schemas/documents/newsletter.tsx
@@ -1,31 +1,65 @@
-import React from 'react'
+import moment from 'moment'
+import { RiNewspaperLine } from 'react-icons/ri'
+
+interface Selection {
+  media: string
+  subtitle: string
+  title: string
+}
 
 export default {
   name: 'newsletter',
   title: 'Newsletter',
   type: 'document',
-  icon: () => (
-    <svg
-      stroke="currentColor"
-      fill="currentColor"
-      strokeWidth="0"
-      viewBox="0 0 24 24"
-      height="1em"
-      width="1em"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g>
-        <path fill="none" d="M0 0h24v24H0z"></path>
-        <path fillRule="nonzero" d="M16 20V4H4v15a1 1 0 0 0 1 1h11zm3 2H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v7h4v9a3 3 0 0 1-3 3zm-1-10v7a1 1 0 0 0 2 0v-7h-2zM6 6h6v6H6V6zm2 2v2h2V8H8zm-2 5h8v2H6v-2zm0 3h8v2H6v-2z"></path>
-      </g>
-    </svg>
-  ),
+  icon: RiNewspaperLine,
   fields: [
     {
       name: 'title',
-      title: 'Page title',
-      type: 'string',
-      group: 'page'
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'logo',
+      title: 'Logo',
+      type: 'image',
+      options: {
+        hotspot: true
+      },
+    },
+    {
+      name: 'headline',
+      title: 'Headline',
+      type: 'text',
+      rows: 3
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true
+      },
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'portableText'
     }
-  ]
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      subtitle: '_createdAt',
+      media: 'mainImage'
+    },
+    prepare(selection: Selection) {
+      const { media, subtitle, title } = selection
+      return {
+        title: title,
+        subtitle: moment(subtitle).format('D MMM YYYY'),
+        media: media
+      }
+    }
+  }
 }
