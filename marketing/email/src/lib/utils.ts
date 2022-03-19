2c44ce2998497967806b6799da4eb0468897d96a
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-19 19:47:00 +0000

    refactor: switch from data-fns to moment and remove unused modules

--- marketing/email/src/lib/utils.ts
+++ marketing/email/src/lib/utils.ts
@@ -1,26 +1,5 @@
-import htm from "htm"
-import vhtml from "vhtml"
-import { uriLooksSafe } from "@portabletext/to-html"
 import imageUrlBuilder from "@sanity/image-url"
 import sanityClient from "./sanityClient"
-
-const html = htm.bind(vhtml)
-export const portableTextComponents = {
-  marks: {
-    link: ({children, value}) => {
-      const href = value.href || ""
-      if (uriLooksSafe(href)) {
-        const rel = href.startsWith("/") ? undefined : "noreferrer"
-        return html`
-          <a href="${href}" rel="${rel}">
-            ${children}
-          </a>
-        `
-      }
-      return children
-    }
-  }
-}
 
 export const urlFor = source => {
   return imageUrlBuilder(sanityClient).image(source)
