b494bce15dc6c06ded74b3244f73216de4714243
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-19 19:46:15 +0000

    refactor: update project info

--- marketing/preview/lib/sanityClient.ts
+++ marketing/preview/lib/sanityClient.ts
@@ -1,8 +1,8 @@
 import sanityClient from "@sanity/client"
 
 export default sanityClient({
-  projectId: "2l9k48uy",
-  dataset: "production",
-  apiVersion: "2022-03-05",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: "2022-03-17",
   useCdn: true
 })
