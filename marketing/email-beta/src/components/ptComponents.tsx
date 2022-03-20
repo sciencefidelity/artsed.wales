df051abf9632f079855c0b567d001d767f6af292
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-20 11:40:30 +0000

    refactor: try putting all styles inline

--- marketing/email-beta/src/components/ptComponents.tsx
+++ marketing/email-beta/src/components/ptComponents.tsx
@@ -4,7 +4,22 @@ import { PortableTextComponents } from "@portabletext/react"
 export const components: PortableTextComponents = {
   block: {
     normal: ({ children }: any) => {
-      return <p className="m-0 py-5 px-0 sans font-200 leading-6">{children}</p>
+      return (
+        <p
+          className="m-0 py-5 px-0 sans font-200 leading-6"
+          style={{
+            margin: "0px",
+            paddingTop: "5px",
+            paddingRight: "0px",
+            paddingBottom: "5px",
+            paddingLeft: "0px",
+            fontFamily: "system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Helvetica,Arial,Verdana,sans-serif !important",
+            lineHeight: "1.5em"
+          }}
+        >
+          {children}
+        </p>
+      )
     }
   },
   marks: {
@@ -15,6 +30,10 @@ export const components: PortableTextComponents = {
           href={value?.href}
           target={target} rel={target === "_blank" ? "noreferrer" : undefined}
           className="text-accent no-underline hover:underline"
+          style={{
+            color: "#00ABD6",
+            textDecoration: "none"
+          }}
         >{children}</a>
       )
     }
