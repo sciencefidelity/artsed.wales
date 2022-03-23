208652f2aa60e4ce328a7b91ead0767833976d7b
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-23 12:14:32 +0000

    feat: try to post on Twitter with the api

--- alpha/pages/api/twitter.ts
+++ alpha/pages/api/twitter.ts
@@ -4,16 +4,16 @@ const client = new TwitterApi(process.env.TWITTER_TOKEN)
 
 // const roClient = client.readOnly
 
-async function getUser(req, res) {
-  const response = await client.v2.userByUsername("artsed_wales", {
-    "user.fields": ["public_metrics"]
-  })
-  const { data } = response
-  const { followers_count } = data.public_metrics
-  console.log(data)
-  res.status(200).json({ followers_count })
-}
-export default getUser
+// async function getUser(req, res) {
+//   const response = await client.v2.userByUsername("artsed_wales", {
+//     "user.fields": ["public_metrics"]
+//   })
+//   const { data } = response
+//   const { followers_count } = data.public_metrics
+//   console.log(data)
+//   res.status(200).json({ followers_count })
+// }
+// export default getUser
 
 // async function getTweet(req, res) {
 //   const tweet = await client.v2.singleTweet("20")
@@ -21,3 +21,9 @@ export default getUser
 //   res.status(200).json({ tweet })
 // }
 // export default getTweet
+
+async function sendTweet(req, res) {
+  const { data: createdTweet } = await client.v2.tweet("Wondering about the name change? We've joined with @EdauCymru and @NAWRcymru to serve all Wales.")
+  res.status(200).json({ createdTweet })
+}
+export default sendTweet
