a524f84aceb9251a9935beb49f3ffb4da7c4d431
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-23 10:10:42 +0000

    feat: connect to twitter api and get follower count

---
+++ alpha/pages/api/twitter.ts
@@ -0,0 +1,23 @@
+import { TwitterApi } from "twitter-api-v2"
+
+const client = new TwitterApi(process.env.TWITTER_TOKEN)
+
+// const roClient = client.readOnly
+
+async function getUser(req, res) {
+  const response = await client.v2.userByUsername("artsed_wales", {
+    "user.fields": ["public_metrics"]
+  })
+  const { data } = response
+  const { followers_count } = data.public_metrics
+  console.log(data)
+  res.status(200).json({ followers_count })
+}
+export default getUser
+
+// async function getTweet(req, res) {
+//   const tweet = await client.v2.singleTweet("20")
+//   console.log(tweet)
+//   res.status(200).json({ tweet })
+// }
+// export default getTweet
