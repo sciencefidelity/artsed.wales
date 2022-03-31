de2a6b7e48fc22310b876e4cb8ed82488d9d9cea
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-30 23:57:19 -0200

    chore: upgrade and migrate Astro

--- marketing/email/astro.config.mjs
+++ marketing/email/astro.config.mjs
@@ -1,8 +1,6 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
 
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-	// Comment out "renderers: []" to enable Astro's default component support.
-	renderers: ["@astrojs/renderer-react"]
-});
+export default defineConfig({
+  integrations: [react()]
+})
