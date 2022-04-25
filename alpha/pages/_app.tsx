c22e24331668db09265878f0ae2b2e9ade31bb65
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-04-25 19:47:42 +0100

    refactor: change to Tailwind style css preflight

--- alpha/pages/_app.tsx
+++ alpha/pages/_app.tsx
@@ -1,8 +1,5 @@
 import { AppProps } from "next/app"
-import "sanitize.css/sanitize.css"
-import "sanitize.css/assets.css"
-import "sanitize.css/reduce-motion.css"
-import "sanitize.css/typography.css"
+import "modern-normalize"
 import "styles/global.scss"
 
 const App = ({ Component, pageProps }: AppProps) =>
