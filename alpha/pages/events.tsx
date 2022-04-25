6ffa98c97d74507e1e383d2a9f027aaf8fd6280f
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-04-24 23:21:38 -0300

    refactor: try to clear filters and uncheck checkboxes with a button

--- alpha/pages/events.tsx
+++ alpha/pages/events.tsx
@@ -24,11 +24,6 @@ interface CheckboxEvent {
   }
 }
 
-interface CheckedItems {
-  type: string
-  id: string
-}
-
 export const getStaticProps: GetStaticProps = async () => {
   const data = await sanityClient.fetch(eventsQuery)
   return {
@@ -39,6 +34,8 @@ export const getStaticProps: GetStaticProps = async () => {
 const Home = ({ data }) => {
   const [filteredEvents, setFilteredEvents] = useState<Event[]>([])
   const [checkedItems, setCheckedItems] = useState<string[]>([])
+  const [checked, setChecked] = useState(false)
+
   const { locale } = useRouter()
   const {
     artforms,
@@ -63,6 +60,7 @@ const Home = ({ data }) => {
   }, [])
 
   const handleChange = (e: CheckboxEvent) => {
+    setChecked(c => !c)
     if (e.target.checked) {
       return setCheckedItems(prev => [...prev, e.target.id])
     }
@@ -71,8 +69,13 @@ const Home = ({ data }) => {
     }
   }
 
+  const clearFilters = () => {
+    if (checked) {
+      setChecked(false)
+    }
+  }
+
   useEffect(() => {
-    console.log(checkedItems)
     if (checkedItems.length === 0) {
       setFilteredEvents(events)
       return
@@ -84,6 +87,17 @@ const Home = ({ data }) => {
     })
     setFilteredEvents(filtered)
   }, [checkedItems])
+
+  useEffect(() => {
+    if (!checked) {
+      setFilteredEvents(events)
+      setCheckedItems([])
+      return
+    }
+    if (checked) {
+      return
+    }
+  }, [checked])
 
   const facilitatorsSorted = facilitators.sort((a, b) => {
     return a.title.split(" ").pop().localeCompare(b.title.split(" ").pop())
@@ -125,6 +139,9 @@ const Home = ({ data }) => {
         </section>
         <div>
           <h2>Filters</h2>
+          <button
+            onClick={() => clearFilters()}
+          >Clear Filters</button>
           <section>
             <h3>Key Stage</h3>
             {keystages && keystages.map(item =>
