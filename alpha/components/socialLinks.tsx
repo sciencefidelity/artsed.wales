3a3b5f16c57e7253ffb714c06ed381e216a5d116
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-04-28 11:10:54 +0100

    refactor: add colors for social icons and remove unused images

--- alpha/components/socialLinks.tsx
+++ alpha/components/socialLinks.tsx
@@ -39,13 +39,37 @@ const SocialLinks: FC<Props> = ({ settings }) => {
     }
   }
 
+  const background = (network: string): string => {
+    switch (network) {
+    case "facebook":
+      return "#1778F2"
+    case "instagram":
+      return "#E4405F"
+    case "linkedin":
+      return "#0A66C2"
+    case "pinterest":
+      return "#c8232c"
+    case "youtube":
+      return "#c4342d"
+    case "twitter":
+      return "#1DA1F2"
+    default:
+      return "#54BE93"
+    }
+  }
+
   return (
     <section className={`${s.socialLinks} ${u.uppercase}`}>
       <ul className={`${s.socialLinksMenu} ${u.flex}`}>
         {settings.social.map(link =>
           <li key={link._key}>
             <a href={link.url}>
-              <div  className={`${s.socialIcon}`}>{icon(link.name)}</div>
+              <div
+                className={`${s.socialIcon}`}
+                style={{
+                  backgroundColor: background(link.name)
+                }}
+              >{icon(link.name)}</div>
             </a>
           </li>
         )}
