bef6ced121534ec725fa73d751a30348888be223
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-13 16:08:08 +0100

    feat: localize plausable script

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,14 +1,19 @@
 import { AppProps } from "next/app"
 import Script from "next/script"
+import { useRouter } from "next/router"
 import "modern-normalize"
 import "styles/global.scss"
 
-const App = ({ Component, pageProps }: AppProps) =>
-  <>
-    <Script
-      src="https://plausible.io/js/plausible.js"
-      data-domain="artsed.wales"
-    />
-    <Component {...pageProps} />
-  </>
+const App = ({ Component, pageProps }: AppProps) => {
+  const { locale } = useRouter()
+  return (
+    <>
+      <Script
+        src="https://plausible.io/js/plausible.js"
+        data-domain={locale === "cy" ? "celfadd.cymru" : "artsed.wales"}
+      />
+      <Component {...pageProps} />
+    </>
+  )
+}
 export default App
