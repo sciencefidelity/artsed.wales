c52967c64b9521a5de3db774095c57752774b8ec
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-13 16:02:18 +0100

    feat: add plausable script

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps } from "next/app"
+import Script from "next/script"
 import "modern-normalize"
 import "styles/global.scss"
 
 const App = ({ Component, pageProps }: AppProps) =>
-  <Component {...pageProps} />
+  <>
+    <Script
+      src="https://plausible.io/js/plausible.js"
+      data-domain="artsed.wales"
+    />
+    <Component {...pageProps} />
+  </>
 export default App
