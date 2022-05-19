51b4c492a5e3694ab17f10c046c4322621848102
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-19 14:57:59 +0100

    fix: work on showing 404 for localized dynamic routes

--- alpha/pages/_error.tsx
+++ alpha/pages/_error.tsx
@@ -1,22 +1,47 @@
-import React from "react"
-import { NextPage } from "next"
+import { GetStaticProps } from "next"
+import sanityClient from "lib/sanityClient"
+import { Layout } from "components/layout"
+import { Localize } from "components/localize"
+import { indexQuery } from "lib/queries"
+import { Company, Label, Navigation, Settings } from "lib/interfaces"
 
-interface ErrorProps {
-  statusCode: number
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await sanityClient.fetch(indexQuery)
+  return {
+    props: { data }
+  }
 }
 
-const Error: NextPage<ErrorProps> = ({ statusCode }) => {
+const Error = ({ data }) => {
+  const { company, labels, navigation, settings } = data as {
+    company: Company
+    labels: Label[]
+    navigation: Navigation
+    settings: Settings
+  }
   return (
-    <div>
-      <h1>Error: {statusCode}</h1>
-    </div>
+    <Layout
+      company={company}
+      labels={labels}
+      navigation={navigation}
+      settings={settings}
+    >
+      <div
+        className="container"
+        style={{
+          height: "100vh",
+          display: "grid",
+          textAlign: "center"
+        }}
+      >
+        <div style={{ margin: "auto" }}>
+          <h1 style={{ fontSize: "3rem", fontWeight: 600 }}>
+            <Localize data={labels[28].text} />
+          </h1>
+          <p><Localize data={labels[29].text} /></p>
+        </div>
+      </div>
+    </Layout>
   )
 }
-
-Error.getInitialProps = ({ res }) => {
-  const statusCode = res?.statusCode || 500
-
-  return { statusCode }
-}
-
 export default Error
