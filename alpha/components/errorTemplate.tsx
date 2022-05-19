1e8510218333ad4658780b74ea88a311c9f9c012
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-19 13:11:43 +0100

    refactor: change error template to spinner

--- alpha/components/errorTemplate.tsx
+++ alpha/components/errorTemplate.tsx
@@ -1,4 +1,4 @@
-import { LinkTo } from "components/linkTo"
+import s from "styles/error.module.scss"
 
 export const ErrorTemplate = () => (
   <div
@@ -10,11 +10,18 @@ export const ErrorTemplate = () => (
     }}
   >
     <div style={{ margin: "auto" }}>
-      <h1>404 not found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      <LinkTo href="/">
-        <p>Home</p>
-      </LinkTo>
+      <svg
+        stroke="currentColor"
+        fill="currentColor"
+        strokeWidth="0"
+        viewBox="0 0 16 16"
+        height="2em"
+        width="2em"
+        className={s.spinner}
+      >
+        <path d="M6 2c0-1.105 0.895-2 2-2s2 0.895 2 2c0 1.105-0.895 2-2 2s-2-0.895-2-2zM10.243 3.757c0-1.105 0.895-2 2-2s2 0.895 2 2c0 1.105-0.895 2-2 2s-2-0.895-2-2zM13 8c0-0.552 0.448-1 1-1s1 0.448 1 1c0 0.552-0.448 1-1 1s-1-0.448-1-1zM11.243 12.243c0-0.552 0.448-1 1-1s1 0.448 1 1c0 0.552-0.448 1-1 1s-1-0.448-1-1zM7 14c0 0 0 0 0 0 0-0.552 0.448-1 1-1s1 0.448 1 1c0 0 0 0 0 0 0 0.552-0.448 1-1 1s-1-0.448-1-1zM2.757 12.243c0 0 0 0 0 0 0-0.552 0.448-1 1-1s1 0.448 1 1c0 0 0 0 0 0 0 0.552-0.448 1-1 1s-1-0.448-1-1zM2.257 3.757c0 0 0 0 0 0 0-0.828 0.672-1.5 1.5-1.5s1.5 0.672 1.5 1.5c0 0 0 0 0 0 0 0.828-0.672 1.5-1.5 1.5s-1.5-0.672-1.5-1.5zM0.875 8c0-0.621 0.504-1.125 1.125-1.125s1.125 0.504 1.125 1.125c0 0.621-0.504 1.125-1.125 1.125s-1.125-0.504-1.125-1.125z">
+        </path>
+      </svg>
     </div>
   </div>
 )
