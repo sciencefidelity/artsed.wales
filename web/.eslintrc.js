692786a7dba171619b76301b8951a96269475aaa
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-13 19:48:08 +0100

    build: add stricter eslint rules and change formatting rules

---
+++ web/.eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  plugins: ["@typescript-eslint", "eslint-comments", "promise", "unicorn"],
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:eslint-comments/recommended",
+    "plugin:promise/recommended",
+    "plugin:unicorn/recommended",
+    "plugin:@next/next/recommended",
+    "prettier",
+  ],
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    // https://basarat.gitbook.io/typescript/main-1/defaultisbad
+    "import/prefer-default-export": "off",
+    // Next uses default exports for routing pages
+    "import/no-default-export": "off",
+    // It's not accurate in the monorepo style
+    "import/no-extraneous-dependencies": "off",
+    // Allow most functions to rely on type inference
+    // @typescript-eslint/explicit-module-boundary-types
+    // will ensure exports are typed
+    "@typescript-eslint/explicit-function-return-type": "off",
+    // React adds the import in compile stage since v17
+    // https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
+    "react/react-in-jsx-scope": "off",
+    // Next `_app.tsx` uses this
+    "react/jsx-props-no-spreading": "off",
+    // Airbnb prefers forEach
+    "unicorn/no-array-for-each": "off",
+    // This was flagging filenames like `next-env.d.ts`
+    "unicorn/prevent-abbreviations": "off",
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        // Allow CJS until ESM support improves
+        "@typescript-eslint/no-var-requires": "off",
+        "unicorn/prefer-module": "off",
+      },
+    },
+  ],
+};
