6202e6d3921c58bded67955fc4fad5f46aae3f37
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-13 20:02:15 +0100

    refactor: fix eslint problems for index component with stricter linting config

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next"
+import { GetStaticProps, NextPage } from "next"
 import { useRouter } from "next/router"
 import { PortableText } from "@portabletext/react"
 import { components } from "components/portableTextComponents"
@@ -11,93 +11,96 @@ import { QuoteSection } from "components/quoteSection"
 import { VideoPlayer } from "components/videoPlayer"
 import { indexQuery } from "lib/queries"
 import {
-	Company,
-	Engagement,
-	Event,
-	Label,
-	Navigation,
-	Page,
-	Quote,
-	Settings,
-	Video
+  Company,
+  Engagement,
+  Event,
+  Label,
+  Navigation,
+  Page,
+  Quote,
+  Settings,
+  Video,
 } from "lib/interfaces"
 import u from "styles/utils.module.scss"
 import s from "styles/index.module.scss"
 import p from "styles/patterns.module.scss"
 
-export const getStaticProps: GetStaticProps = async () => {
-	const data = await sanityClient.fetch(indexQuery)
-	return {
-		props: { data }
-	}
+interface Data {
+  company: Company
+  engagement: Engagement
+  events: Event[]
+  labels: Label[]
+  navigation: Navigation
+  pages: Page[]
+  quotes: Quote[]
+  settings: Settings
+  videos: Video[]
 }
 
-const Home = ({ data }) => {
-	const { locale } = useRouter()
-	const {
-		company,
-		engagement,
-		events,
-		labels,
-		navigation,
-		pages,
-		quotes,
-		settings,
-		videos
-	} = data as {
-		company: Company
-		engagement: Engagement
-		events: Event[]
-		labels: Label[]
-		navigation: Navigation
-		pages: Page[]
-		quotes: Quote[]
-		settings: Settings
-		videos: Video[]
-	}
+export const getStaticProps: GetStaticProps = async () => {
+  const data: Data = await sanityClient.fetch(indexQuery)
+  return {
+    props: { data },
+  }
+}
 
-	return (
-		<Layout
-			company={company}
-			labels={labels}
-			navigation={navigation}
-			settings={settings}
-		>
-			<div>
-				<section className={`${s.hero} ${p.lines}`}>
-					<h2
-						className={`${s.heroText}`}
-						dangerouslySetInnerHTML={{
-							__html: localize(labels[21].text, locale)
-						}}
-					/>
-				</section>
-				<div className={`${u.container}`}>
-					<section>
-						<article className={`${s.indexContent}`}>
-							<PortableText
-								value={
-									locale === "cy" && pages[0].__i18n_refs
-										? pages[0].__i18n_refs.body
-										: pages[0].body
-								}
-								components={components}
-							/>
-						</article>
-						<VideoPlayer video={videos[0]} />
-					</section>
-					<hr />
-					<EngagementSection engagement={engagement} />
-					<QuoteSection label={labels[27]} quotes={quotes} />
-					<hr />
-					<FeaturedEvents
-						events={events.filter(event => event.feature)}
-						label={labels[26]}
-					/>
-					<hr />
-				</div>
-			</div>
-		</Layout>
-	)
+const Home: NextPage = ({ data }: { data: Data }) => {
+  const { locale } = useRouter()
+  const {
+    company,
+    engagement,
+    events,
+    labels,
+    navigation,
+    pages,
+    quotes,
+    settings,
+    videos,
+  } = data
+
+  return (
+    <Layout
+      company={company}
+      labels={labels}
+      navigation={navigation}
+      settings={settings}
+    >
+      <div>
+        <section className={`${s.hero} ${p.lines}`}>
+          <h2
+            className={`${s.heroText}`}
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: localize(labels[21].text, locale),
+            }}
+          />
+        </section>
+        <div className={`${u.container}`}>
+          <section>
+            <article className={`${s.indexContent}`}>
+              <PortableText
+                value={
+                  locale === "cy" && pages[0].__i18n_refs
+                    ? pages[0].__i18n_refs.body
+                    : pages[0].body
+                }
+                components={components}
+              />
+            </article>
+            <VideoPlayer video={videos[0]} />
+          </section>
+          <hr />
+          <EngagementSection engagement={engagement} />
+          <QuoteSection label={labels[27]} quotes={quotes} />
+          <hr />
+          <FeaturedEvents
+            events={events.filter((event) => event.feature)}
+            label={labels[26]}
+          />
+          <hr />
+        </div>
+      </div>
+    </Layout>
+  )
 }
 export default Home
