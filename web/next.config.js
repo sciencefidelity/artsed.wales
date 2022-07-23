db6db1d1e6dd63f58a9e7d6d3ddefe25be65d62c
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-23 21:53:56 -0200

    refactor: reformat

--- web/next.config.js
+++ web/next.config.js
@@ -1,37 +1,39 @@
 /** @type {import("next").NextConfig} */
-module.exports = {
-	async headers() {
-		return [
-			{
-				// matching all API routes
-				source: "/api/sendgrid",
-				headers: [
-					{ key: "Content-Type", value: "application/json" },
-					{ key: "Access-Control-Allow-Credentials", value: "true" },
-					{ key: "Access-Control-Allow-Origin", value: "http://sendgrid.com" },
-					{ key: "Access-Control-Allow-Methods", value: "POST, GET, OPTIONS" },
-					{ key: "Access-Control-Allow-Headers", value: "X-Requested-With" }
-				]
-			}
-		]
-	},
-	i18n: {
-		locales: ["en", "cy"],
-		defaultLocale: "en",
-		localeDetection: false,
-		domains: [
-			{
-				domain: "artsed.wales",
-				defaultLocale: "en"
-			},
-			{
-				domain: "celfadd.cymru",
-				defaultLocale: "cy"
-			}
-		]
-	},
-	images: {
-		domains: ["cdn.sanity.io"]
-	},
-	swcMinify: true
-}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/sendgrid",
+        headers: [
+          { key: "Content-Type", value: "application/json" },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "http://sendgrid.com" },
+          { key: "Access-Control-Allow-Methods", value: "POST, GET, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-Requested-With" },
+        ],
+      },
+    ];
+  },
+  i18n: {
+    locales: ["en", "cy"],
+    defaultLocale: "en",
+    localeDetection: false,
+    domains: [
+      {
+        domain: "artsed.wales",
+        defaultLocale: "en",
+      },
+      {
+        domain: "celfadd.cymru",
+        defaultLocale: "cy",
+      },
+    ],
+  },
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
