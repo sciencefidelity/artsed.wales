bebbe5862cec84a565e123dfdf68acea84e689b4
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-26 22:21:10 -0200

    refactor: prettify

--- resources/src/lib/queries.ts
+++ resources/src/lib/queries.ts
@@ -1,7 +1,7 @@
 import groq from "groq"
 
 export const resourceQuery = groq`{
-	"resources": *[_type == "resource"]{
-		body[], image, "slug": slug.current, title
-		}
+  "resources": *[_type == "resource"]{
+    body[], image, "slug": slug.current, title
+    }
 }`
