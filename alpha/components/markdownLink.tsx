3dd3417ee53a38b3caee2bf8b98669c5740dc7a7
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-28 00:56:54 +0100

    refactor: reorganise file structure

--- alpha/components/markdownLink.tsx
+++
@@ -1,5 +0,0 @@
-export const MarkdownLink = ({ children, href }) => (
-  <a href={href} target="_blank" rel="noopener noreferrer">
-    {children}
-  </a>
-)
--- alpha/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import { AppProps } from "next/app"
-import "modern-normalize"
-import "styles/global.scss"
-
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
-export default App
